bbc4b44de1001280af0ba54aa782330c8008fded
hemanth <hemanthchowdary@0766@gmail.com>
2025-09-05 14:20:39 -0500

    Update all project files

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-from src.cnnClassifier import logger
 
-logger.info("welcome to our custom log ")
+
+# If cnnClassifier is a local package, add its parent directory to PYTHONPATH
+
+# If cnnClassifier is a local package, set PYTHONPATH in your terminal before running this script:
+# export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+# Or, set it in Python as follows:
+# import os
+# os.environ['PYTHONPATH'] = f"{os.environ.get('PYTHONPATH', '')}:{os.getcwd()}"
+
+
+
+# Or, if cnnClassifier is a pip-installable package, install it
+# Make sure to install cnnClassifier using pip in your terminal before running this script.
+
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
